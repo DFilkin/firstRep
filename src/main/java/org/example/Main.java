2b700ac3f12d7c567f4f6ce095ebef4b5a6e82fb
DFilkin <denis.dobryy1st@yandex.ru>
2025-02-18 23:59:48 +0300

    4 try to docker deploy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world Linux!");
+        SpringApplication.run(Main.class, args);
+    }
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/")
+        public String hello() {
+            return "Hello world from Docker!";
+        }
     }
 }
